0f68dc755a3fb890a0e2aad1f82c2067c81a5042
artem <artemaliev1993@gmail.com>
2019-07-10 21:52:24 +0300

    Added func for listing notes.

--- book/views.py
+++ book/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Note
 
 # Create your views here.
-
-
+def notes_list(request):
+    notes = Note.objects.get.all()
+    return render(request, 'book/index.html', context={'notes':notes})
