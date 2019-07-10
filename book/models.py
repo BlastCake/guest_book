f941ed89eb0b64d8647420c45c994bcb44fc6812
artem <artemaliev1993@gmail.com>
2019-07-10 21:16:46 +0300

    initial commit

---
+++ book/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+
+class Note(models.Model):
+    user_name = models.CharField(max_length=150)
+    slug = models.SlugField(max_length=50, unique=True)
+    email = models.EmailField(max_length=150, unique=True)
+    home_page = models.URLField(max_length=150, blank=True)
+    text = models.TextField(max_length=350)
+    tags = models.ManyToManyField('Tag', blank=True, related_name='notes')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '{} | {} | {}'.format(self.user_name, self.email, self.text)
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50)
+
+    def __str__(self):
+        return self.title
