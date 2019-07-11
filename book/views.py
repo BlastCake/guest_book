0a2eaecdb370d49262773b08a43612947db01e76
artem <artemaliev1993@gmail.com>
2019-07-11 12:13:27 +0300

    Added NoteCreate class.

--- book/views.py
+++ book/views.py
@@ -1,7 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Note
+from django.views.generic import View
+from .forms import NoteForm
+
 
 # Create your views here.
 def notes_list(request):
-    notes = Note.objects.get.all()
+    notes = Note.objects.all()
     return render(request, 'book/index.html', context={'notes':notes})
+
+
+
+class NoteCreate(View):
+    def get(self, request):
+        form = NoteForm()
+        return render(request, 'book/note_create.html', context={'form':form})
+
+    def post(self, request):
+        bound_form = NoteForm(request.POST)
+
+        if bound_form.is_valid():
+            note = bound_form.save()
+            return redirect(notes_list)
+
+        return render(request, 'book/note_create.html', context={'form': bound_form})
+
