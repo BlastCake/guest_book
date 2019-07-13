b9a4f47e6f7946f51ca57a8880c552f82b107b0d
artem <artemaliev1993@gmail.com>
2019-07-13 13:23:26 +0300

    Created TagForm class.

--- book/forms.py
+++ book/forms.py
@@ -6,14 +6,24 @@ from django import forms
 class NoteForm(forms.ModelForm):
     class Meta:
         model = Note
-        fields = ['user_name', 'slug', 'email',
+        fields = ['user_name', 'email',
                   'home_page','text', 'tags']
 
         widgets = {
             'user_name': forms.TextInput(attrs={'class': 'form-control'}),
-            'slug': forms.TextInput(attrs={'class': 'form-control'}),
             'email': forms.TextInput(attrs={'class': 'form-control'}),
             'home_page': forms.TextInput(attrs={'class': 'form-control'}),
             'text': forms.Textarea(attrs={'class': 'form-control'}),
             'tags': forms.SelectMultiple(attrs={'class': 'form-control'})
+        }
+
+
+class TagForm(forms.ModelForm):
+    class Meta:
+        model = Tag
+        fields = ['title', 'slug']
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'slug': forms.TextInput(attrs={'class': 'form-control'})
         }
