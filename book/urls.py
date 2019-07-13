8dcfd600ded56a58590ae514158dffffd88e6c04
artem <artemaliev1993@gmail.com>
2019-07-13 16:33:17 +0300

    Added path variable for tag create func.

--- book/urls.py
+++ book/urls.py
@@ -3,6 +3,9 @@ from .views import *
 
 
 urlpatterns = [
-    path('', notes_list, name='list_notes'),
-    path('create/', NoteCreate.as_view(), name='note_create_url')
+    path('', notes_list, name='notes_list_url'),
+    path('note/create', NoteCreate.as_view(), name='note_create_url'),
+    path('tags', tags_list, name='tags_list_url'),
+    path('tag/create', TagCreate.as_view(), name='tag_create_url'),
+
 ]
