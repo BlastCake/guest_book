cfddd1759a305b64626d58efae32896289bf7236
artem <artemaliev1993@gmail.com>
2019-07-13 16:34:23 +0300

    Added 'tags_list' func.

--- book/views.py
+++ book/views.py
@@ -2,15 +2,23 @@ from django.shortcuts import render, redirect
 from .models import Note
 from django.views.generic import View
 from .utils import ObjectCreateMixin
-from .forms import NoteForm
+from .forms import *
 
 # Create your views here.
 def notes_list(request):
     notes = Note.objects.all()
     return render(request, 'book/index.html', context={'notes':notes})
 
+def tags_list(request):
+    tags = Tag.objects.all()
+    return render(request, 'book/list_tags.html', context={'tags':tags})
 
 
 class NoteCreate(ObjectCreateMixin, View):
     model_form = NoteForm
     template = 'book/note_create.html'
+
+
+class TagCreate(ObjectCreateMixin, View):
+    model_form = TagForm
+    template = 'book/tag_create.html'
